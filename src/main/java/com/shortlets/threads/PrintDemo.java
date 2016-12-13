3ec9db5feada542f2bac76a3a9250e3dffd97c29
fssantos <fssantos>
2016-12-13 14:08:30 -0200

    classes novas

--- src/main/java/com/shortlets/threads/PrintDemo.java
+++ src/main/java/com/shortlets/threads/PrintDemo.java
@@ -10,5 +10,14 @@ package com.shortlets.threads;
  * @author fssantos
  */
 public class PrintDemo {
+    public void printCount(){
+        try{
+            for(int i =5; i>0;i++){
+                System.out.println(" Selected number is: " +i);
+            }
+        }catch(Exception e){
+            System.out.println("Thread has been interrupted. ");
+        }
+    }
     
 }
