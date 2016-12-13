2f2220ca4c104c9797eb694bb3207ff02fc6755b
fssantos <fssantos>
2016-12-13 14:00:02 -0200

    classes

---
+++ src/main/java/com/shortlets/threads/ThreadDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shortlets.threads;
+
+/**
+ *
+ * @author fssantos
+ */
+public class ThreadDemo implements Runnable{
+    private Thread thread;
+    private String threadName;
+    
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
